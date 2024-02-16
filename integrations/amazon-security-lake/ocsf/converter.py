7fd25d1213e605ceac965eb3eb31395c05072b3f
Fede Tux <federico.galland@wazuh.com>
2024-02-16 15:28:51 -0300

    Code polish and fix errors

--- integrations/amazon-security-lake/ocsf/converter.py
+++ integrations/amazon-security-lake/ocsf/converter.py
@@ -1,87 +1,89 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
 # event comes from Filebeat
-event = {}
-
+#event = {}
+#print(event)
 
 def normalize(level: int) -> int:
-    """
-    Normalizes rule level into the 0-6 range, required by OCSF.
-    """
-    # TODO normalization
-    return level
+  """
+  Normalizes rule level into the 0-6 range, required by OCSF.
+  """
+  # TODO normalization
+  return level
 
 
 def join(iterable, separator=","):
-    return (separator.join(iterable))
+  return (separator.join(iterable))
 
 
 def convert(event: dict) -> dict:
-    """
-    Converts Wazuh events to OCSF's Detecting Finding (2004) class.
-    """
-    ocsf_class_template = \
+  """
+  Converts Wazuh events to OCSF's Detecting Finding (2004) class.
+  """
+  ocsf_class_template = \
+    {
+      "activity_id": 1,
+      "category_name": "Findings",
+      "category_uid": 2,
+      "class_name": "Detection Finding",
+      "class_uid": 2004,
+      "count": event["_source"]["rule"]["firedtimes"],
+      "message": event["_source"]["rule"]["description"],
+      "finding_info": {
+        "analytic": {
+          "category": join(event["_source"]["rule"]["groups"]),
+          "name": event["_source"]["decoder"]["name"],
+          "type_id": 1,
+          "uid": event["_source"]["rule"]["id"],
+        },
+        "attacks": {
+          "tactic": {
+            #"name": join(event["_source"]["rule"]["mitre"]["tactic"]),
+            "dummy": True
+          },
+          "technique": {
+            #"name": join(event["_source"]["rule"]["mitre"]["technique"]),
+            #"uid": join(event["_source"]["rule"]["mitre"]["id"]),
+            "dummy": True
+          },
+          "version": "v13.1"
+        },
+        "title": event["_source"]["rule"]["description"],
+        "types": [
+          event["_source"]["input"]["type"]
+        ],
+        "uid": event["_source"]['id']
+      },
+      "metadata": {
+        "log_name": "Security events",
+        "log_provider": "Wazuh",
+        "product": {
+          "name": "Wazuh",
+          "lang": "en",
+          "vendor_name": "Wazuh, Inc,."
+        },
+        "version": "1.1.0",
+      },
+      #"raw_data": event["_source"]["full_log"],
+      "resources": [
         {
-            "activity_id": 1,
-            "category_name": "Findings",
-            "category_uid": 2,
-            "class_name": "Detection Finding",
-            "class_uid": 2004,
-            "count": event["rule"]["firedtimes"],
-            "message": event["rule"]["description"],
-            "finding_info": {
-                "analytic": {
-                    "category": join(event["rule"]["groups"]),
-                    "name": event["decoder"]["name"],
-                    "type_id": 1,
-                    "uid": event["rule"]["id"],
-                },
-                "attacks": {
-                    "tactic": {
-                        "name": join(event["rule"]["mitre"]["tactic"]),
-                    },
-                    "technique": {
-                        "name": join(event["rule"]["mitre"]["technique"]),
-                        "uid": join(event["rule"]["mitre"]["id"]),
-                    },
-                    "version": "v13.1"
-                },
-                "title": event["rule"]["description"],
-                "types": [
-                    event["input"]["type"]
-                ],
-                "uid": event['id']
-            },
-            "metadata": {
-                "log_name": "Security events",
-                "log_provider": "Wazuh",
-                "product": {
-                    "name": "Wazuh",
-                    "lang": "en",
-                    "vendor_name": "Wazuh, Inc,."
-                },
-                "version": "1.1.0",
-            },
-            "raw_data": event["full_log"],
-            "resources": [
-                {
-                    "name": event["agent"]["name"],
-                    "uid": event["agent"]["id"]
-                },
-            ],
-            "risk_score": event["rule"]["level"],
-            "severity_id": normalize(event["rule"]["level"]),
-            "status_id": 99,
-            "time": event["timestamp"],
-            "type_uid": 200401,
-            "unmapped": {
-                "data_sources": [
-                    event["_index"],
-                    event["location"],
-                    event["manager"]["name"]
-                ],
-                "nist": event["rule"]["nist_800_53"],  # Array
-            }
-        }
+          "name": event["_source"]["agent"]["name"],
+          "uid": event["_source"]["agent"]["id"]
+        },
+      ],
+      "risk_score": event["_source"]["rule"]["level"],
+      "severity_id": normalize(event["_source"]["rule"]["level"]),
+      "status_id": 99,
+      "time": event["_source"]["timestamp"],
+      "type_uid": 200401,
+      "unmapped": {
+        "data_sources": [
+          #event["_source"]["_index"],
+          event["_source"]["location"],
+          event["_source"]["manager"]["name"]
+        ],
+        #"nist": event["_source"]["rule"]["nist_800_53"],  # Array
+      }
+    }
 
-    return ocsf_class_template
+  return ocsf_class_template

5fcc9a394a339f373a5a7a557bc302b7efcfc42e
Álex Ruiz <alejandro.ruiz.becerra@wazuh.com>
2024-02-22 16:19:33 +0100

    Cleanup

--- integrations/amazon-security-lake/run.py
+++ integrations/amazon-security-lake/run.py
@@ -17,19 +17,9 @@ def _test():
             print("--")
             print("")
             print(ocsf_event)
-        #     event = Event.model_validate_json(json.dumps(event))
-        #     print(event)
-        #     ocsf_event = to_detection_finding(event)
 
         except KeyError as e:
             raise (e)
-        # except ValidationError as e:
-        #     print(e)
-
-        # if ocsf_event:
-        #     with open("wazuh-event.ocsf.json", "w") as fd:
-        #         json.dump(ocsf_event.model_dump(), fd)
-        #         print(ocsf_event.model_dump())
 
 
 if __name__ == '__main__':

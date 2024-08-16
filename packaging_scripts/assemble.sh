a06bed701f75f4bbf08458e04a83f834eb676eb0
Álex Ruiz <alejandro.ruiz.becerra@wazuh.com>
2024-08-16 14:50:54 +0200

    Try new approach to build wazuh-indexer with plugins

--- packaging_scripts/assemble.sh
+++ packaging_scripts/assemble.sh
@@ -232,9 +232,10 @@ function install_plugins() {
     done
 
     echo "Installing Wazuh plugins"
-    indexer_plugins_folder="$HOME/artifacts/plugins"
     for plugin in "${wazuh_plugins[@]}"; do
-        OPENSEARCH_PATH_CONF=$PATH_CONF "${PATH_BIN}/opensearch-plugin" install --batch --verbose "file:${indexer_plugins_folder}/${plugin}-${VERSION}-${REVISION}.zip"
+        plugin_from_maven="org.wazuh.plugin:${plugin}:${VERSION}.${REVISION}"
+        mvn -Dmaven.repo.local="${maven_repo_local}" org.apache.maven.plugins:maven-dependency-plugin:2.1:get -DrepoUrl=https://repo1.maven.org/maven2 -Dartifact="${plugin_from_maven}:zip"
+        OPENSEARCH_PATH_CONF=$PATH_CONF "${PATH_BIN}/opensearch-plugin" install --batch --verbose "file:${maven_repo_local}/org/wazuh/plugin/${plugin}/${VERSION}.${REVISION}/${plugin}-${VERSION}.${REVISION}.zip"
     done
 }
 

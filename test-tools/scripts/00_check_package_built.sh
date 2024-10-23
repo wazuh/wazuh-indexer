1dcd7d3bd6c3750e641be6377fb74f64f94acf1a
quebim <kevin.ledesma@wazuh.com>
2024-10-23 12:24:50 -0300

    Update validations scripts to be able to use the generated package name

--- test-tools/scripts/00_check_package_built.sh
+++ test-tools/scripts/00_check_package_built.sh
@@ -1,61 +1,82 @@
 #!/bin/bash
 
-## SPDX-License-Identifier: Apache-2.0
-## The OpenSearch Contributors require contributions made to
+# SPDX-License-Identifier: Apache-2.0
+# The OpenSearch Contributors require contributions made to
 # this file be licensed under the Apache-2.0 license or a
 # compatible open source license.
 
+# Default package revision
+PKG_REVISION="0"
+
 # Check if the necessary arguments are provided
-if [ "$#" -ne 2 ]; then
-    echo "Usage: $0 <RUN_ID> <PKG_VERSION> <(Optional)PKG_REVISION>"
+if [ "$#" -lt 1 ]; then
+    echo "Usage: $0 <RUN_ID> [-v <PKG_VERSION>] [-r <PKG_REVISION>] [-n <PKG_NAME>]"
     echo
     echo "Parameters:"
     echo "    RUN_ID         The GHA workflow execution ID."
-    echo "    PKG_VERSION    The version of the wazuh-indexer package."
-    echo "    PKG_REVISION   (Optional) The revision of the package. Defaults to 'test' if not provided."
+    echo "    -v             (Optional) The version of the wazuh-indexer package."
+    echo "    -r             (Optional) The revision of the package. Defaults to '0' if not provided."
+    echo "    -n             (Optional) The package name. If not provided, it will be configured based on version and revision."
     echo
     echo "Please ensure you have the GITHUB_TOKEN environment variable set to access the GitHub repository."
-    echo
     exit 1
 fi
 
 RUN_ID=$1
-PKG_VERSION=$2
-PKG_REVISION=${3:-"0"}
-REPO="wazuh/wazuh-indexer"
-URL="https://api.github.com/repos/$REPO/actions/artifacts"
+shift
 
-# Detect OS and architecture
-if [ -f /etc/os-release ]; then
-    . /etc/os-release
-    OS=$(echo $NAME | tr '[:upper:]' '[:lower:]')
-else
-    echo "Unsupported OS."
+while getopts v:r:n: flag
+do
+    case "${flag}" in
+        v) PKG_VERSION=${OPTARG};;
+        r) PKG_REVISION=${OPTARG};;
+        n) PKG_NAME=${OPTARG};;
+        *)
+            echo "Usage: $0 <RUN_ID> [-v <PKG_VERSION>] [-r <PKG_REVISION>] [-n <PKG_NAME>]"
+            exit 1
+            ;;
+    esac
+done
+
+# Validate GITHUB_TOKEN environment variable
+if [ -z "$GITHUB_TOKEN" ]; then
+    echo "Please ensure you have the GITHUB_TOKEN environment variable set to access the GitHub repository."
     exit 1
 fi
 
-ARCH=$(uname -m)
-# Determine package type
-case "$OS" in
-    "ubuntu" | "debian")
-        PKG_FORMAT="deb"
-        [ "$ARCH" == "x86_64" ] && ARCH="amd64"
-        [ "$ARCH" == "aarch64" ] && ARCH="arm64"
-        PKG_NAME="wazuh-indexer_${PKG_VERSION}-${PKG_REVISION}_${ARCH}.${PKG_FORMAT}"
-        ;;
-    "centos" | "fedora" | "rhel" | "red hat enterprise linux")
-        PKG_FORMAT="rpm"
-        PKG_NAME="wazuh-indexer-${PKG_VERSION}-${PKG_REVISION}.${ARCH}.${PKG_FORMAT}"
-        ;;
-    *)
-        echo "Unsupported OS. ${OS}"
-        exit 1
-        ;;
-esac
+# Ensure either PKG_NAME or both PKG_VERSION and PKG_REVISION are provided
+if [ -z "$PKG_NAME" ] && { [ -z "$PKG_VERSION" ] || [ -z "$PKG_REVISION" ]; }; then
+    echo "Error: Either a package name (-n) or both a version (-v) and revision (-r) must be provided."
+    exit 1
+fi
+
+REPO="wazuh/wazuh-indexer"
+URL="https://api.github.com/repos/$REPO/actions/artifacts"
+
+# Determine package type if PKG_NAME is not provided
+if [ -z "$PKG_NAME" ]; then
+    ARCH=$(uname -m)
+    case "$(uname -n)" in
+        "ubuntu" | "debian")
+            PKG_FORMAT="deb"
+            [ "$ARCH" == "x86_64" ] && ARCH="amd64"
+            [ "$ARCH" == "aarch64" ] && ARCH="arm64"
+            PKG_NAME="wazuh-indexer_${PKG_VERSION}-${PKG_REVISION}_${ARCH}.${PKG_FORMAT}"
+            ;;
+        "centos" | "fedora" | "rhel" | "red hat enterprise linux")
+            PKG_FORMAT="rpm"
+            PKG_NAME="wazuh-indexer-${PKG_VERSION}-${PKG_REVISION}.${ARCH}.${PKG_FORMAT}"
+            ;;
+        *)
+            echo "Unsupported OS. ${OS}"
+            exit 1
+            ;;
+    esac
+fi
 
 # Fetch the list of artifacts
 echo "Fetching artifacts list..."
-RESPONSE=$(curl -s -L -H "Accept: application/vnd.github+json" -H "Authorization: Bearer $GITHUB_TOKEN" -H "X-GitHub-Api-Version: 2022-11-28" $URL?name=$PKG_NAME)
+RESPONSE=$(curl -s -L -H "Accept: application/vnd.github+json" -H "Authorization: Bearer $GITHUB_TOKEN" -H "X-GitHub-Api-Version: 2022-11-28" "$URL?name=$PKG_NAME")
 
 # Check if the curl command was successful
 if [ $? -ne 0 ]; then
--- test-tools/scripts/01_check_installation.sh
+++ test-tools/scripts/01_check_installation.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 # SPDX-License-Identifier: Apache-2.0
 # The OpenSearch Contributors require contributions made to
 # this file be licensed under the Apache-2.0 license or a
@@ -7,23 +6,17 @@
 
 # Usage function to display help
 usage() {
-    echo "Usage: $0 <ARTIFACT_ID> <PKG_VERSION> <(Optional)PKG_REVISION>"
+    echo "Usage: $0 <ARTIFACT_ID> [-v <PKG_VERSION>] [-r <PKG_REVISION>] [-n <PKG_NAME>]"
     echo
     echo "Parameters:"
     echo "    ARTIFACT_ID    The unique ID of the GHA artifact."
-    echo "    PKG_VERSION    The version of the wazuh-indexer package."
-    echo "    PKG_REVISION   (Optional) The revision of the package. Defaults to 'test' if not provided."
+    echo "    -v             (Optional) The version of the wazuh-indexer package."
+    echo "    -r             (Optional) The revision of the package. Defaults to '0' if not provided."
+    echo "    -n             (Optional) The package name."
     echo
     echo "Please ensure you have the GITHUB_TOKEN environment variable set to access the GitHub repository."
-    echo
     exit 1
 }
-
-# Check if GITHUB_TOKEN env var is set
-if [ -z "$1" ]; then
-    echo "Error: Environment variable GITHUB_TOKEN is not configured."
-    usage
-fi
 
 # Check if ARTIFACT_ID is provided
 if [ -z "$1" ]; then
@@ -31,15 +24,33 @@ if [ -z "$1" ]; then
     usage
 fi
 
-# Check if PKG_VERSION is provided
-if [ -z "$2" ]; then
-    echo "Error: PKG_VERSION not provided."
+ARTIFACT_ID=$1
+shift
+
+while getopts v:r:n: flag
+do
+    case "${flag}" in
+        v) PKG_VERSION=${OPTARG};;
+        r) PKG_REVISION=${OPTARG};;
+        n) PKG_NAME=${OPTARG};;
+        *)
+            usage
+            ;;
+    esac
+done
+
+# Validate GITHUB_TOKEN environment variable
+if [ -z "$GITHUB_TOKEN" ]; then
+    echo "Error: Environment variable GITHUB_TOKEN is not configured."
     usage
 fi
 
-ARTIFACT_ID=$1
-PKG_VERSION=$2
-PKG_REVISION=${3:-"0"}
+# Ensure either PKG_NAME or both PKG_VERSION and PKG_REVISION are provided
+if [ -z "$PKG_NAME" ] && { [ -z "$PKG_VERSION" ] || [ -z "$PKG_REVISION" ]; }; then
+    echo "Error: Either a package name (-n) or both a version (-v) and revision (-r) must be provided."
+    usage
+fi
+
 REPO="wazuh/wazuh-indexer"
 URL="https://api.github.com/repos/${REPO}/actions/artifacts/${ARTIFACT_ID}/zip"
 
@@ -52,26 +63,26 @@ else
     exit 1
 fi
 
-ARCH=$(uname -m)
-# Determine package type
-case "$OS" in
-    "ubuntu" | "debian")
-        PKG_FORMAT="deb"
-        [ "$ARCH" == "x86_64" ] && ARCH="amd64"
-        [ "$ARCH" == "aarch64" ] && ARCH="arm64"
-        # Construct package name
-        PKG_NAME="wazuh-indexer_${PKG_VERSION}-${PKG_REVISION}_${ARCH}.${PKG_FORMAT}"
-        ;;
-    "centos" | "fedora" | "rhel" | "red hat enterprise linux")
-        PKG_FORMAT="rpm"
-        # Construct package name
-        PKG_NAME="wazuh-indexer-${PKG_VERSION}-${PKG_REVISION}.${ARCH}.${PKG_FORMAT}"
-        ;;
-    *)
-        echo "Unsupported OS."
-        exit 1
-        ;;
-esac
+# Determine package type if PKG_NAME is not provided
+if [ -z "$PKG_NAME" ]; then
+    ARCH=$(uname -m)
+    case "$(uname -n)" in
+        "ubuntu" | "debian")
+            PKG_FORMAT="deb"
+            [ "$ARCH" == "x86_64" ] && ARCH="amd64"
+            [ "$ARCH" == "aarch64" ] && ARCH="arm64"
+            PKG_NAME="wazuh-indexer_${PKG_VERSION}-${PKG_REVISION}_${ARCH}.${PKG_FORMAT}"
+            ;;
+        "centos" | "fedora" | "rhel" | "red hat enterprise linux")
+            PKG_FORMAT="rpm"
+            PKG_NAME="wazuh-indexer-${PKG_VERSION}-${PKG_REVISION}.${ARCH}.${PKG_FORMAT}"
+            ;;
+        *)
+            echo "Unsupported OS."
+            exit 1
+            ;;
+    esac
+fi
 
 # Download the package
 echo "Downloading wazuh-indexer package from GitHub artifactory..."
@@ -80,6 +91,7 @@ curl -L -H "Accept: application/vnd.github+json" \
     -H "Authorization: Bearer $GITHUB_TOKEN" \
     -H "X-GitHub-Api-Version: 2022-11-28" \
     $URL -o package.zip > /dev/null 2>&1
+
 if [ $? -ne 0 ]; then
     echo "Error downloading package."
     exit 1
@@ -90,6 +102,7 @@ echo "Package downloaded successfully"
 echo "Decompressing wazuh-indexer package..."
 unzip ./package.zip
 rm package.zip
+
 if [ $? -ne 0 ]; then
     echo "Error unzipping package."
     exit 1
@@ -106,6 +119,7 @@ case "$PKG_FORMAT" in
         sudo rpm -i $PKG_NAME
         ;;
 esac
+
 if [ $? -ne 0 ]; then
     echo "Error installing package."
     exit 1

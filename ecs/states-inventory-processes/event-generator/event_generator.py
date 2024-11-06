6bdc325cd9844dd57ef31e497f7b4012573f93ae
quebim <kevin.ledesma@wazuh.com>
2024-11-06 15:31:44 -0300

    Add event generator tool for inventory-processes index

---
+++ ecs/states-inventory-processes/event-generator/event_generator.py
@@ -0,0 +1,138 @@
+#!/bin/python3
+
+import datetime
+import json
+import logging
+import random
+import requests
+import warnings
+
+# Constants and Configuration
+LOG_FILE = 'generate_data.log'
+GENERATED_DATA_FILE = 'generatedData.json'
+DATE_FORMAT = "%Y-%m-%dT%H:%M:%S.%fZ"
+# Default values
+INDEX_NAME = "wazuh-states-inventory-processes"
+USERNAME = "admin"
+PASSWORD = "admin"
+IP = "127.0.0.1"
+PORT = "9200"
+
+# Configure logging
+logging.basicConfig(filename=LOG_FILE, level=logging.INFO)
+
+# Suppress warnings
+warnings.filterwarnings("ignore")
+
+
+def generate_random_date():
+    start_date = datetime.datetime.now()
+    end_date = start_date - datetime.timedelta(days=10)
+    random_date = end_date + (start_date - end_date) * random.random()
+    return random_date.strftime(DATE_FORMAT)
+
+
+def generate_random_agent():
+    agent = {
+        'id': f'agent{random.randint(0, 99)}',
+        'groups': [f'group{random.randint(0, 99)}', f'group{random.randint(0, 99)}']
+    }
+    return agent
+
+
+def generate_random_process():
+    process = {
+        'args': f'args{random.randint(0, 999)}',
+        'command_line': f'command_line{random.randint(0, 999)}',
+        'group': {
+            'id': f'group_id{random.randint(0, 999)}'
+        },
+        'name': f'process{random.randint(0, 999)}',
+        'parent': {
+            'pid': random.randint(0, 99999)
+        },
+        'pid': random.randint(0, 99999),
+        'real_group': {
+            'id': f'real_group_id{random.randint(0, 999)}'
+        },
+        'real_user': {
+            'id': f'real_user_id{random.randint(0, 999)}'
+        },
+        'saved_group': {
+            'id': f'saved_group_id{random.randint(0, 999)}'
+        },
+        'saved_user': {
+            'id': f'saved_user_id{random.randint(0, 999)}'
+        },
+        'start': generate_random_date(),
+        'thread': {
+            'id': random.randint(0, 99999)
+        },
+        'user': {
+            'id': f'user_id{random.randint(0, 999)}'
+        }
+    }
+    return process
+
+
+def generate_random_data(number):
+    data = []
+    for _ in range(number):
+        event_data = {
+            '@timestamp': datetime.datetime.now().strftime(DATE_FORMAT),
+            'agent': generate_random_agent(),
+            'process': generate_random_process()
+        }
+        data.append(event_data)
+    return data
+
+
+def inject_events(ip, port, index, username, password, data):
+    url = f'https://{ip}:{port}/{index}/_doc'
+    session = requests.Session()
+    session.auth = (username, password)
+    session.verify = False
+    headers = {'Content-Type': 'application/json'}
+
+    try:
+        for event_data in data:
+            response = session.post(url, json=event_data, headers=headers)
+            if response.status_code != 201:
+                logging.error(f'Error: {response.status_code}')
+                logging.error(response.text)
+                break
+        logging.info('Data injection completed successfully.')
+    except Exception as e:
+        logging.error(f'Error: {str(e)}')
+
+
+def main():
+    try:
+        number = int(input("How many events do you want to generate? "))
+    except ValueError:
+        logging.error("Invalid input. Please enter a valid number.")
+        return
+
+    logging.info(f"Generating {number} events...")
+    data = generate_random_data(number)
+
+    with open(GENERATED_DATA_FILE, 'a') as outfile:
+        for event_data in data:
+            json.dump(event_data, outfile)
+            outfile.write('\n')
+
+    logging.info('Data generation completed.')
+
+    inject = input(
+        "Do you want to inject the generated data into your indexer? (y/n) ").strip().lower()
+    if inject == 'y':
+        ip = input(f"Enter the IP of your Indexer (default: '{IP}'): ") or IP
+        port = input(f"Enter the port of your Indexer (default: '{PORT}'): ") or PORT
+        index = input(f"Enter the index name (default: '{INDEX_NAME}'): ") or INDEX_NAME
+        username = input(f"Username (default: '{USERNAME}'): ") or USERNAME
+        password = input(f"Password (default: '{PASSWORD}'): ") or PASSWORD
+        inject_events(ip, port, index, username, password, data)
+
+
+if __name__ == "__main__":
+    main()

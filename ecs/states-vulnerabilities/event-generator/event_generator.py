d9623b0bb0ac3b126574e54b885845ffa0838a8c
Fede Galland <99492720+f-galland@users.noreply.github.com>
2024-11-15 14:03:02 -0300

    Migrate event generators for 5.x indices to master (#553)

--- ecs/states-vulnerabilities/event-generator/event_generator.py
+++ ecs/states-vulnerabilities/event-generator/event_generator.py
@@ -1,32 +1,28 @@
 #!/bin/python3
 
-# This script generates sample events and injects them into the Wazuh Indexer.
-# The events follow the Elastic Common Schema (ECS) format, and contains the following fields:
-#   - agent
-#   - package
-#   - host
-#   - vulnerability
-#   - wazuh (custom)
-#
-# This is an ad-hoc script for the vulnerability module. Extend to support other modules.
-
 import datetime
-import random
 import json
-import requests
-import warnings
 import logging
+import random
+import requests
+import urllib3
 
 # Constants and Configuration
 LOG_FILE = 'generate_data.log'
 GENERATED_DATA_FILE = 'generatedData.json'
 DATE_FORMAT = "%Y-%m-%dT%H:%M:%S.%fZ"
+# Default values
+INDEX_NAME = "wazuh-states-vulnerabilities"
+USERNAME = "admin"
+PASSWORD = "admin"
+IP = "127.0.0.1"
+PORT = "9200"
 
 # Configure logging
 logging.basicConfig(filename=LOG_FILE, level=logging.INFO)
 
 # Suppress warnings
-warnings.filterwarnings("ignore")
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 
 def generate_random_date():
@@ -38,145 +34,138 @@ def generate_random_date():
 
 def generate_random_agent():
     agent = {
-        'build': {'original': f'build{random.randint(0, 9999)}'},
         'id': f'agent{random.randint(0, 99)}',
         'name': f'Agent{random.randint(0, 99)}',
+        'type': random.choice(['filebeat', 'windows', 'linux', 'macos']),
         'version': f'v{random.randint(0, 9)}-stable',
-        'ephemeral_id': f'{random.randint(0, 99999)}',
-        'type': random.choice(['filebeat', 'windows', 'linux', 'macos'])
+        'groups': [f'group{random.randint(0, 99)}', f'group{random.randint(0, 99)}'],
+        'host': generate_random_host()
     }
     return agent
 
 
-def generate_random_event():
-    event = {
-        'action': random.choice(['login', 'logout', 'create', 'delete', 'modify', 'read', 'write', 'upload', 'download',
-                                 'copy', 'paste', 'cut', 'move', 'rename', 'open', 'close', 'execute', 'run', 'install',
-                                 'uninstall', 'start', 'stop', 'kill', 'suspend', 'resume', 'sleep', 'wake', 'lock',
-                                 'unlock', 'encrypt', 'decrypt', 'compress', 'decompress', 'archive', 'unarchive',
-                                 'mount', 'unmount', 'eject', 'connect', 'disconnect', 'send', 'receive']),
-        'agent_id_status': random.choice(['verified', 'mismatch', 'missing', 'auth_metadata_missing']),
-        'category': random.choice(['authentication', 'authorization', 'configuration', 'communication', 'file',
-                                  'network', 'process', 'registry', 'storage', 'system', 'web']),
-        'code': f'{random.randint(0, 99999)}',
-        'created': generate_random_date(),
-        'dataset': random.choice(['process', 'file', 'registry', 'socket', 'dns', 'http', 'tls', 'alert',
-                                 'authentication', 'authorization', 'configuration', 'communication', 'file',
-                                  'network', 'process', 'registry', 'storage', 'system', 'web']),
-        'duration': random.randint(0, 99999),
-        'end': generate_random_date(),
-        'hash': str(hash(f'hash{random.randint(0, 99999)}')),
-        'id': f'{random.randint(0, 99999)}',
-        'ingested': generate_random_date(),
-        'kind': random.choice(['alert', 'asset', 'enrichment', 'event', 'metric',
-                              'state', 'pipeline_error', 'signal']),
-        'module': random.choice(['process', 'file', 'registry', 'socket', 'dns', 'http', 'tls', 'alert',
-                                'authentication', 'authorization', 'configuration', 'communication', 'file',
-                                 'network', 'process', 'registry', 'storage', 'system', 'web']),
-        'original': f'original{random.randint(0, 99999)}',
-        'outcome': random.choice(['success', 'failure', 'unknown']),
-        'provider': random.choice(['process', 'file', 'registry', 'socket', 'dns', 'http', 'tls', 'alert',
-                                  'authentication', 'authorization', 'configuration', 'communication', 'file',
-                                   'network', 'process', 'registry', 'storage', 'system', 'web']),
-        'reason': f'This event happened due to reason{random.randint(0, 99999)}',
-        'reference': f'https://system.example.com/event/#{random.randint(0, 99999)}',
-        'risk_score': round(random.uniform(0, 10), 1),
-        'risk_score_norm': round(random.uniform(0, 10), 1),
-        'sequence': random.randint(0, 10),
-        'severity': random.randint(0, 10),
-        'start': generate_random_date(),
-        'timezone': random.choice(['UTC', 'GMT', 'PST', 'EST', 'CST', 'MST', 'PDT', 'EDT', 'CDT', 'MDT']),
-        'type': random.choice(['access', 'admin', 'allowed', 'change', 'connection', 'creation', 'deletion',
-                               'denied', 'end', 'error', 'group', 'indicator', 'info', 'installation', 'protocol',
-                               'start', 'user']),
-        'url': f'http://mysystem.example.com/alert/{random.randint(0, 99999)}'
-    }
-    return event
-
-
 def generate_random_host():
-    family = random.choice(
-        ['debian', 'ubuntu', 'macos', 'ios', 'android', 'RHEL'])
-    version = f'{random.randint(0, 99)}.{random.randint(0, 99)}'
     host = {
+        'architecture': random.choice(['x86_64', 'arm64']),
+        'boot': {
+            'id': f'bootid{random.randint(0, 9999)}'
+        },
+        'cpu': {
+            'usage': random.uniform(0, 100)
+        },
+        'disk': {
+            'read': {
+                'bytes': random.randint(1000, 1000000)
+            },
+            'write': {
+                'bytes': random.randint(1000, 1000000)
+            }
+        },
+        'domain': f'domain{random.randint(0, 1000)}',
+        'geo': generate_random_geo(),
+        'hostname': f'host{random.randint(0, 1000)}',
+        'id': f'id{random.randint(0, 1000)}',
+        'ip': f'{random.randint(1, 255)}.{random.randint(0, 255)}.{random.randint(0, 255)}.{random.randint(0, 255)}',
+        'mac': f'{random.randint(0, 255):02x}:{random.randint(0, 255):02x}:{random.randint(0, 255):02x}:{random.randint(0, 255):02x}:{random.randint(0, 255):02x}:{random.randint(0, 255):02x}',
+        'name': f'host{random.randint(0, 1000)}',
+        'network': {
+            'egress': {
+                'bytes': random.randint(1000, 1000000),
+                'packets': random.randint(100, 10000)
+            },
+            'ingress': {
+                'bytes': random.randint(1000, 1000000),
+                'packets': random.randint(100, 10000)
+            }
+        },
         'os': {
-            'full': f'{family} {version}',
-            'kernel': f'{version}kernel{random.randint(0, 99)}',
-            'name': f'{family} {version}',
-            'platform': family,
-            'type': random.choice(['windows', 'linux', 'macos', 'ios', 'android', 'unix']),
-            'version': version
-        }
+            'family': random.choice(['debian', 'ubuntu', 'macos', 'ios', 'android', 'RHEL']),
+            'full': f'{random.choice(["debian", "ubuntu", "macos", "ios", "android", "RHEL"])} {random.randint(0, 99)}.{random.randint(0, 99)}',
+            'kernel': f'{random.randint(0, 9)}.{random.randint(0, 9)}.{random.randint(0, 9)}',
+            'name': random.choice(['Linux', 'Windows', 'macOS']),
+            'platform': random.choice(['platform1', 'platform2']),
+            'type': random.choice(['os_type1', 'os_type2']),
+            'version': f'{random.randint(0, 9)}.{random.randint(0, 9)}.{random.randint(0, 9)}'
+        },
+        'pid_ns_ino': f'pid_ns{random.randint(0, 9999)}',
+        'risk': {
+            'calculated_level': random.choice(['low', 'medium', 'high']),
+            'calculated_score': random.uniform(0, 10),
+            'calculated_score_norm': random.uniform(0, 1),
+            'static_level': random.choice(['low', 'medium', 'high']),
+            'static_score': random.uniform(0, 10),
+            'static_score_norm': random.uniform(0, 1)
+        },
+        'type': random.choice(['type1', 'type2']),
+        'uptime': random.randint(1000, 1000000)
     }
     return host
 
 
-def generate_random_labels():
-    labels = {
-        'label1': f'label{random.randint(0, 99)}', 'label2': f'label{random.randint(0, 99)}'}
-    return labels
+def generate_random_geo():
+    geo = {
+        'city_name': 'CityName',
+        'continent_code': 'NA',
+        'continent_name': 'North America',
+        'country_iso_code': 'US',
+        'country_name': 'United States',
+        'location': {
+            'lat': round(random.uniform(-90, 90), 6),
+            'lon': round(random.uniform(-180, 180), 6)
+        },
+        'name': f'location{random.randint(0, 999)}',
+        'postal_code': f'{random.randint(10000, 99999)}',
+        'region_iso_code': 'US-CA',
+        'region_name': 'California',
+        'timezone': 'America/Los_Angeles'
+    }
+    return geo
 
 
 def generate_random_package():
     package = {
-        'architecture': random.choice(['x86', 'x64', 'arm', 'arm64']),
+        'architecture': random.choice(['x86_64', 'arm64']),
         'build_version': f'build{random.randint(0, 9999)}',
         'checksum': f'checksum{random.randint(0, 9999)}',
         'description': f'description{random.randint(0, 9999)}',
-        'install_scope': random.choice(['user', 'system']),
+        'install_scope': random.choice(['system', 'user']),
         'installed': generate_random_date(),
-        'license': f'license{random.randint(0, 9)}',
-        'name': f'name{random.randint(0, 99)}',
-        'path': f'/path/to/package{random.randint(0, 99)}',
-        'reference': f'package-reference-{random.randint(0, 99)}',
-        'size': random.randint(0, 99999),
-        'type': random.choice(['deb', 'rpm', 'msi', 'pkg', 'app', 'apk', 'exe', 'zip', 'tar', 'gz', '7z',
-                              'rar', 'cab', 'iso', 'dmg', 'tar.gz', 'tar.bz2', 'tar.xz', 'tar.Z', 'tar.lz4',
-                               'tar.sz', 'tar.zst']),
-        'version': f'v{random.randint(0, 9)}-stable'
+        'license': random.choice(['GPL', 'MIT', 'Apache']),
+        'name': f'package{random.randint(0, 9999)}',
+        'path': f'/path/to/package{random.randint(0, 9999)}',
+        'reference': f'reference{random.randint(0, 9999)}',
+        'size': random.randint(1000, 100000),
+        'type': random.choice(['deb', 'rpm']),
+        'version': f'{random.randint(0, 9)}.{random.randint(0, 9)}.{random.randint(0, 9)}'
     }
     return package
 
 
 def generate_random_vulnerability():
-    id = random.randint(0, 9999)
     vulnerability = {
-        'category': random.choice(['security', 'config', 'os', 'package', 'custom']),
-        'classification': [f'classification{random.randint(0, 9999)}'],
+        'category': random.choice(['security', 'compliance']),
+        'classification': f'classification{random.randint(0, 9999)}',
         'description': f'description{random.randint(0, 9999)}',
-        'enumeration': 'CVE',
-        'id': f'CVE-{id}',
-        'reference': f'https://mycve.test.org/cgi-bin/cvename.cgi?name={id}',
-        'report_id': f'report-{random.randint(0, 9999)}',
+        'detected_at': generate_random_date(),
+        'enumeration': f'enumeration{random.randint(0, 9999)}',
+        'id': f'id{random.randint(0, 9999)}',
+        'published_at': generate_random_date(),
+        'reference': f'reference{random.randint(0, 9999)}',
+        'report_id': f'report{random.randint(0, 9999)}',
         'scanner': {
-            'vendor': f'vendor-{random.randint(0, 9)}',
-            'source': random.choice(['NVD', 'OpenCVE', 'OpenVAS', 'Tenable'])
+            'source': random.choice(['Nessus', 'OpenVAS']),
+            'vendor': random.choice(['Tenable', 'Greenbone'])
         },
         'score': {
-            'base': round(random.uniform(0, 10), 1),
-            'environmental': round(random.uniform(0, 10), 1),
-            'temporal': round(random.uniform(0, 10), 1),
-            'version': round(random.uniform(0, 10), 1)
+            'base': random.uniform(0, 10),
+            'environmental': random.uniform(0, 10),
+            'temporal': random.uniform(0, 10),
+            'version': random.choice(['v2', 'v3'])
         },
-        'severity': random.choice(['Low', 'Medium', 'High', 'Critical']),
-        'detected_at': generate_random_date(),
-        'published_at': generate_random_date(),
+        'severity': random.choice(['low', 'medium', 'high']),
         'under_evaluation': random.choice([True, False])
     }
     return vulnerability
-
-
-def generate_random_wazuh():
-    wazuh = {
-        'cluster': {
-            'name': f'wazuh-cluster-{random.randint(0,10)}',
-            'node': f'wazuh-cluster-node-{random.randint(0,10)}'
-        },
-        'schema': {
-            'version': '1.7.0'
-        },
-    }
-    return wazuh
 
 
 def generate_random_data(number):
@@ -186,8 +175,7 @@ def generate_random_data(number):
             'agent': generate_random_agent(),
             'host': generate_random_host(),
             'package': generate_random_package(),
-            'vulnerability': generate_random_vulnerability(),
-            'wazuh': generate_random_wazuh()
+            'vulnerability': generate_random_vulnerability()
         }
         data.append(event_data)
     return data
@@ -214,7 +202,7 @@ def inject_events(ip, port, index, username, password, data):
 
 def main():
     try:
-        number = int(input("How many events do you want to generate? ").strip() or 50)
+        number = int(input("How many events do you want to generate? "))
     except ValueError:
         logging.error("Invalid input. Please enter a valid number.")
         return
@@ -232,11 +220,11 @@ def main():
     inject = input(
         "Do you want to inject the generated data into your indexer? (y/n) ").strip().lower()
     if inject == 'y':
-        ip = input("Enter the IP of your Indexer: ").strip() or "localhost"
-        port = input("Enter the port of your Indexer: ").strip() or 9200
-        index = input("Enter the index name: ").strip() or "wazuh-states-vulnerability-test"
-        username = input("Username: ").strip() or "admin"
-        password = input("Password: ").strip()
+        ip = input(f"Enter the IP of your Indexer (default: '{IP}'): ") or IP
+        port = input(f"Enter the port of your Indexer (default: '{PORT}'): ") or PORT
+        index = input(f"Enter the index name (default: '{INDEX_NAME}'): ") or INDEX_NAME
+        username = input(f"Username (default: '{USERNAME}'): ") or USERNAME
+        password = input(f"Password (default: '{PASSWORD}'): ") or PASSWORD
         inject_events(ip, port, index, username, password, data)
 
 

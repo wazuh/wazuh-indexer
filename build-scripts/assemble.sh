404878e27968766ad03a2c0a6881e42552d5312b
Alex Ruiz <alejandro.ruiz.becerra@wazuh.com>
2024-11-22 21:31:35 +0100

    Remove extra new line

--- build-scripts/assemble.sh
+++ build-scripts/assemble.sh
@@ -354,7 +354,6 @@ function assemble_deb() {
     cp "distribution/packages/src/deb/Makefile" "${TMP_DIR}"
     cp "distribution/packages/src/deb/debmake_install.sh" "${TMP_DIR}"
     cp -r "distribution/packages/src/deb/debian" "${TMP_DIR}"
-
     chmod a+x "${TMP_DIR}/debmake_install.sh"
     # Copy performance analyzer service file
     enable_performance_analyzer

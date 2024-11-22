6d2c2361d68ba5cf910d1cf5fcb2d45509b00b15
Alex Ruiz <alejandro.ruiz.becerra@wazuh.com>
2024-11-22 21:29:21 +0100

    Use upstream debian scripts

--- build-scripts/assemble.sh
+++ build-scripts/assemble.sh
@@ -354,9 +354,7 @@ function assemble_deb() {
     cp "distribution/packages/src/deb/Makefile" "${TMP_DIR}"
     cp "distribution/packages/src/deb/debmake_install.sh" "${TMP_DIR}"
     cp -r "distribution/packages/src/deb/debian" "${TMP_DIR}"
-    cp -r "distribution/packages/src/common/scripts/." "${TMP_DIR}/debian"
-    echo "CONTENT OF ${TMP_DIR}/debian"
-    ls -la "${TMP_DIR}/debian"
+
     chmod a+x "${TMP_DIR}/debmake_install.sh"
     # Copy performance analyzer service file
     enable_performance_analyzer
@@ -369,8 +367,16 @@ function assemble_deb() {
 
     # Extract min-package. Creates usr/, etc/ and var/ in the current directory
     echo "Extract ${ARTIFACT_BUILD_NAME} archive"
+    # Extracts code
     ar xf "${ARTIFACT_BUILD_NAME}" data.tar.gz
     tar zvxf data.tar.gz
+    # Extracts debian control files (preinst, postrm, ...)
+    # We need to use some custom files, otherwise debuild fails
+    mv "./debian/control" "./debian/wazuh-control"
+    ar xf "${ARTIFACT_BUILD_NAME}" control.tar.gz
+    tar zvxf control.tar.gz -C "debian"
+    mv "./debian/wazuh-control" "./debian/control"
+    rm "./debian/conffiles"
 
     local version
     version=$(cat ./usr/share/wazuh-indexer/VERSION)

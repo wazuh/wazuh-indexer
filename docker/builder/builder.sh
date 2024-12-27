64c57f86df480c935e041e8b6295a7473e0b024d
Álex Ruiz <alejandro.ruiz.becerra@wazuh.com>
2024-12-27 13:06:42 +0100

    Export missing variables to the Docker container to build packages (#620)
    * Export missing variables to the Docker container to build packages
    
    * Remove 'set -x' flag

--- docker/builder/builder.sh
+++ docker/builder/builder.sh
@@ -103,6 +103,12 @@ function main() {
     VERSION=$(cat VERSION)
     export REPO_PATH
     export VERSION
+    export INDEXER_PLUGINS_BRANCH
+    export INDEXER_REPORTING_BRANCH
+    export REVISION
+    export IS_STAGE
+    export DISTRIBUTION
+    export ARCHITECTURE
 
     parse_args "${@}"
 

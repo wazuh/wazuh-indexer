f10414be5e48729890c3898b3768cf727e306217
Jorgesnchz <jorge.sanchez@wazuh.com>
2025-02-26 14:59:14 +0100

    Fixed a typo error in build-scripts/build-and-push-docker-image.sh

--- build-scripts/build-and-push-docker-image.sh
+++ build-scripts/build-and-push-docker-image.sh
@@ -79,7 +79,7 @@ function parse_args() {
     fi
 
     REVISION="${REVISION:-0}"
-    VERSION=$(bash build-scripts/product_name.sh)
+    VERSION=$(bash build-scripts/product_version.sh)
 }
 
 # ====

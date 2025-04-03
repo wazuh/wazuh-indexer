90e8148e02ba77b2a29d90822abb8e71cec6c82e
Jorgesnchz <jorge.sanchez@wazuh.com>
2025-04-03 12:49:24 +0200

    test with -tt

--- packaging_scripts/connect.sh
+++ packaging_scripts/connect.sh
@@ -17,4 +17,4 @@ private_key=$(grep 'ansible_ssh_private_key_file:' "$file" | awk '{print $2}')
 # =======
 # Connect to the machine
 # =======
-ssh -i $private_key -p $port ${user}@${host}
+ssh -i $private_key -p $port -tt ${user}@${host}

42bf979212ed90ab4f22cd3e38f7a682934286b2
ana-rm <ana.rejano@wazuh.com>
2026-03-09 10:01:02 +0100

    Migrate rpm dependencies to provision script and update runners

--- build-scripts/provision.sh
+++ build-scripts/provision.sh
@@ -7,4 +7,12 @@ apt-get update -y && apt-get upgrade -y && apt-get install -y curl build-essenti
    apt-get install -y debmake debhelper-compat &&
    apt-get install -y libxrender1 libxtst6 libxi6 &&
    apt-get install -y libatk1.0-0 libatk-bridge2.0-0 libcups2 libdrm2 libatspi2.0-dev libxcomposite-dev libxdamage1 libxfixes3 libxfixes-dev libxrandr2 libgbm-dev libxkbcommon-x11-0 libpangocairo-1.0-0 libcairo2 libcairo2-dev libnss3 libnspr4 libnspr4-dev jq &&
-   apt-get clean -y
+
+
+# Docker installation
+apt-get update
+apt-get install -y docker.io
+systemctl start docker || true
+chmod 666 /var/run/docker.sock || true
+
+apt-get clean -y

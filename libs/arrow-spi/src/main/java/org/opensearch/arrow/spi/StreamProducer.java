e8d8a7a2d048402106aa541fa44a56d7c7542dce
Jorge Sanchez <jorge.sanchez@wazuh.com>
2026-04-06 11:07:53 +0200

    Fix error with Java doc

--- libs/arrow-spi/src/main/java/org/opensearch/arrow/spi/StreamProducer.java
+++ libs/arrow-spi/src/main/java/org/opensearch/arrow/spi/StreamProducer.java
@@ -31,7 +31,6 @@ import java.io.Closeable;
  *     private final SearchRequest searchRequest;
  *     private static final int BATCH_SIZE = 1000;
  *
- *     @Override
  *     public VectorSchemaRoot createRoot(BufferAllocator allocator) {
  *         List<Field> fields = Arrays.asList(
  *             Field.nullable("id", FieldType.valueOf(MinorType.VARCHAR)),
@@ -40,10 +39,8 @@ import java.io.Closeable;
  *         return VectorSchemaRoot.create(new Schema(fields), allocator);
  *     }
  *
- *     @Override
  *     public BatchedJob createJob(BufferAllocator allocator) {
  *         return new BatchedJob() {
- *             @Override
  *             public void run(VectorSchemaRoot root, FlushSignal flushSignal) {
  *                 SearchResponse response = client.search(searchRequest);
  *                 int currentBatch = 0;
